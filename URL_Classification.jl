b96e241294d4fd247823e5f1841cdbac669f1db6
Michal Bělohlávek <belohmi3@cvut.cz>
2023-12-08 08:43:58 +0100

    Finalization of optimalization of fill_dict function

--- URL_Classification.jl
+++ URL_Classification.jl
@@ -1,4 +1,4 @@
-using CSV, DataFrames, Flux, Mill, URIs, Cthulhu, ProfileView
+using CSV, DataFrames, Flux, Mill, URIs, Cthulhu, ProfileView, BenchmarkTools
 
 parts_of_url = ["fragment", "query", "path"]
 function main()
@@ -6,7 +6,6 @@ function main()
     # Read lines
     lines = readlines("mixed_lines.txt") # TODO: Read only 20% of lines 
     size_to_alloc = size(lines)[1] # Amount of lines loaded
-    print(lines[1])
     ## TODO: Prelalocate sizes 
     # Dictionary for storage of part of URLS (Remainder: dictionaries automatically resize) 
     dict = Dict{String,Vector{Vector{String}}}()
@@ -16,56 +15,50 @@ function main()
 
     # Prealocating size for the Vector of Vectors of Strings
     for key in parts_of_url
-        dict[key] = Vector{Vector{String}}(undef, size_to_alloc)
+        dict[key] = Vector{Vector{String}}(undef, size_to_alloc) 
     end
-
-
+    
     # TODO: Prealocate size for the rest
     dict_hashed = Dict{String,Vector{Vector{Vector{UInt64}}}}()
     dict_hashed["fragment"] = []
     dict_hashed["query"] = []
     dict_hashed["path"] = []
 
-
     bag_indx_parts = Dict{String, Vector{Int64}}()
     bag_indx_parts["fragment"] = []
     bag_indx_parts["query"] = []
     bag_indx_parts["path"] = []
+    
+    fill_dict(dict, bag_indx_parts, lines)
+    #ProfileView.@profview fill_dict(dict, bag_indx_parts, lines)
+    #descend(fill_dict,Tuple{Dict{String, Vector{Vector{String}}}, Dict{String, Vector{Int64}}, Vector{String}})
+    #@descend fill_dict(dict, bag_indx_parts, lines)
 end
 
+# Fill dict with URL parts and fill bag_indx_parts with lenghts of parts of url
+function fill_dict(D::Dict{String, Vector{Vector{String}}}, E::Dict{String, Vector{Int64}}, lines::Vector{String})
+    # TODO: Prealocate the vectros of vectors
+    #D["path"] = URIs.splitpath.(URI.(lines))
+    map!(vecs->vecs, D["path"], URIs.splitpath.(URI.(lines)))
+    #D["query"] = vectorize_queryparams.(URIs.queryparams.(URI.(lines)))
+    map!(vecs->vecs, D["query"], vectorize_queryparams.(URIs.queryparams.(URI.(lines))))
+
+    # Alocate memory for E dict - bag_indx_parts, map lengths of parts of URLs into a dictionary vector
+    E["path"] = Vector{Int64}(undef, length(D["path"]))
+    map!(numb->numb, E["path"], length.(D["path"]))
+
+    E["query"] = Vector{Int64}(undef, length(D["query"]))
+    map!(numb->numb, E["query"], length.(D["query"]))
+end
+
+# Creating a vector of key+value from dictionary
+function vectorize_queryparams(X::Dict{String, String})
+    vector_positions = Vector{String}(undef, length(X))
+    map!(key -> string(key, "=", X[key]), vector_positions, collect(keys(X)))
+    return vector_positions
+end
 
 main()
-
-
-
-# TODO: Remove pushes
-# Add url to dict and add lengths of part of the url to the bag_indx_parts
-function add_url_to_dict(x::URI, dict::Dict{String, Vector{Vector{String}}}, indx::Dict{String, Vector{Int64}})
-    path = string.(URIs.splitpath(x.path))
-    push!(get!(dict, "path", Vector{String}), path)
-    push!(indx["path"], length(path))
-    
-    query = [string(pair[1], "=", pair[2]) for pair in pairs(queryparams(x))]
-    push!(get!(dict, "query", Vector{string}), query)
-    push!(indx["query"], length(keys(queryparams(x))))
-    
-    fragment = string.(x.fragment)
-    push!(get!(dict, "fragment", Vector{string}), [fragment])
-    push!(indx["fragment"], length(fragment)) 
-end
-
-
-# Transforming vector of words into a vector of trigrams
-function transform_to_trig(x::Vector{String})
-    trig_vec = Vector{Vector{UInt64}}()
-    for elem in x
-        cur = [elem[i:i+2] for i=1:length(elem)-2]
-        a = map(hash,cur).%2053 #hash trigrams and modulo them
-        push!(trig_vec, a)
-    end
-    return trig_vec
-end
-
 
 # Mapping hashed vector onto a histogram vector
 function map_to_vec(x::Vector{UInt64})
@@ -79,24 +72,18 @@ function map_to_vec(x::Vector{UInt64})
     return map_vec
 end
 
-# Fill dict with URL parts and fill bag_indx_parts with lenghts of parts of url
-function fill_dict(D::Dict{String, Vector{Vector{String}}}, E::Dict{String, Vector{Int}}, lines::Vector{String})
-    
-
-
-
-    for line in lines
-        add_url_to_dict(URI(line), D, E)
-        i += 1
-        if(i > 10)
-            break
-        end
+# Transforming vector of words into a vector of trigrams
+function transform_to_trig(x::Vector{String})
+    trig_vec = Vector{Vector{UInt64}}()
+    for elem in x
+        cur = [elem[i:i+2] for i=1:length(elem)-2]
+        a = map(hash,cur).%2053 #hash trigrams and modulo them
+        push!(trig_vec, a)
     end
+    return trig_vec
 end
 
-#ProfileView.@profview fill_dict(dict, bag_indx_parts,"mixed_lines.txt")
-#fill_dict(dict, bag_indx_parts,"mixed_lines.txt")
-#print(dict)
+
 function trig_map(D::Dict{String, Vector{Vector{String}}}, E::Dict{String, Vector{Vector{Vector{UInt64}}}})
     # Keep track of lengths of part of URL somehow
     for key in keys(D)
@@ -152,13 +139,13 @@ function create_range(x::Vector{Int64})
     return vector_to_ret
 end
 
-bag_path = create_bags(dict_hashed["path"], "path")
-bag_query = create_bags(dict_hashed["query"], "query")
-bag_fragment = create_bags(dict_hashed["fragment"], "fragment")
+#bag_path = create_bags(dict_hashed["path"], "path")
+#bag_query = create_bags(dict_hashed["query"], "query")
+#bag_fragment = create_bags(dict_hashed["fragment"], "fragment")
 
-bag_path_index = create_range(bag_indx_parts["path"])
-bag_query_index = create_range(bag_indx_parts["query"])
-bag_fragment_index = create_range(bag_indx_parts["fragment"])
+#bag_path_index = create_range(bag_indx_parts["path"])
+#bag_query_index = create_range(bag_indx_parts["query"])
+#bag_fragment_index = create_range(bag_indx_parts["fragment"])
 
 # Vector of how many parts is the URL consisting of 
 # ??
@@ -175,4 +162,4 @@ function bag_ind_of_bag()
 end
 
 # Waste?
-#bag_bag_indx = create_range(bag_ind_of_bag())
+#bag_bag_indx = create_range(bag_ind_of_bag()
