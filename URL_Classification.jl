b5cf7d9f1c3b766d4617e3620e72e818d3894b73
Michal Bělohlávek <belohmi3@cvut.cz>
2023-12-09 15:58:10 +0100

    Transform to trig function vectorized

--- URL_Classification.jl
+++ URL_Classification.jl
@@ -1,18 +1,15 @@
 using CSV, DataFrames, Flux, Mill, URIs, Cthulhu, ProfileView, BenchmarkTools
 
-parts_of_url = ["fragment", "query", "path"]
+parts_of_url = ["query", "path"]
 function main()
-
     # Read lines
     lines = readlines("mixed_lines.txt") # TODO: Read only 20% of lines 
     size_to_alloc = size(lines)[1] # Amount of lines loaded
     ## TODO: Prelalocate sizes 
     # Dictionary for storage of part of URLS (Remainder: dictionaries automatically resize) 
     dict = Dict{String,Vector{Vector{String}}}()
-    dict["fragment"] = []
     dict["query"] = []
     dict["path"] = []
-
     # Prealocating size for the Vector of Vectors of Strings
     for key in parts_of_url
         dict[key] = Vector{Vector{String}}(undef, size_to_alloc) 
@@ -20,12 +17,14 @@ function main()
     
     # TODO: Prealocate size for the rest
     dict_hashed = Dict{String,Vector{Vector{Vector{UInt64}}}}()
-    dict_hashed["fragment"] = []
     dict_hashed["query"] = []
     dict_hashed["path"] = []
 
+    for key in parts_of_url
+        dict_hashed[key] = Vector{Vector{String}}(undef, size_to_alloc) 
+    end
+
     bag_indx_parts = Dict{String, Vector{Int64}}()
-    bag_indx_parts["fragment"] = []
     bag_indx_parts["query"] = []
     bag_indx_parts["path"] = []
     
@@ -33,14 +32,22 @@ function main()
     #ProfileView.@profview fill_dict(dict, bag_indx_parts, lines)
     #descend(fill_dict,Tuple{Dict{String, Vector{Vector{String}}}, Dict{String, Vector{Int64}}, Vector{String}})
     #@descend fill_dict(dict, bag_indx_parts, lines)
+
+
+    # Doesn't work through mapping for some reason
+    for key in parts_of_url
+        for i=1:length(dict[key])
+            dict_hashed[key][i] = transform_to_trig(dict[key][i])
+        end
+    end
 end
+
 
 # Fill dict with URL parts and fill bag_indx_parts with lenghts of parts of url
 function fill_dict(D::Dict{String, Vector{Vector{String}}}, E::Dict{String, Vector{Int64}}, lines::Vector{String})
     # TODO: Prealocate the vectros of vectors
     #D["path"] = URIs.splitpath.(URI.(lines))
     map!(vecs->vecs, D["path"], URIs.splitpath.(URI.(lines)))
-    #D["query"] = vectorize_queryparams.(URIs.queryparams.(URI.(lines)))
     map!(vecs->vecs, D["query"], vectorize_queryparams.(URIs.queryparams.(URI.(lines))))
 
     # Alocate memory for E dict - bag_indx_parts, map lengths of parts of URLs into a dictionary vector
@@ -50,7 +57,6 @@ function fill_dict(D::Dict{String, Vector{Vector{String}}}, E::Dict{String, Vect
     E["query"] = Vector{Int64}(undef, length(D["query"]))
     map!(numb->numb, E["query"], length.(D["query"]))
 end
-
 # Creating a vector of key+value from dictionary
 function vectorize_queryparams(X::Dict{String, String})
     vector_positions = Vector{String}(undef, length(X))
@@ -58,11 +64,26 @@ function vectorize_queryparams(X::Dict{String, String})
     return vector_positions
 end
 
+# Transforming vector of words into a vector of trigrams
+function transform_to_trig(x::Vector{String})
+    trig_vec = Vector{Vector{UInt64}}(undef, length(x)) # Initialize vector to a length of the whole string
+    # Initialize internal vectors 
+    for i = 1:length(trig_vec)
+        trig_vec[i] = Vector{UInt64}(undef, length(x[i])-2) # for each vector within - Initialize to length of string
+    end
+    # For every string in x, create trigram, hash it, map it into a vector on position[ind]
+    for (ind, val) in enumerate(x)
+        map!(trig->trig, trig_vec[ind], hash.([val[i:i+2] for i=1:length(val)-2]))
+    end
+    return trig_vec
+end
+
+
 main()
 
 # Mapping hashed vector onto a histogram vector
 function map_to_vec(x::Vector{UInt64})
-    map_vec = zeros(UInt64, 2053)
+    map_vec = Vector{UInt64}(undef,2053)
     for numb in x
         if numb==0
             continue
@@ -72,45 +93,7 @@ function map_to_vec(x::Vector{UInt64})
     return map_vec
 end
 
-# Transforming vector of words into a vector of trigrams
-function transform_to_trig(x::Vector{String})
-    trig_vec = Vector{Vector{UInt64}}()
-    for elem in x
-        cur = [elem[i:i+2] for i=1:length(elem)-2]
-        a = map(hash,cur).%2053 #hash trigrams and modulo them
-        push!(trig_vec, a)
-    end
-    return trig_vec
-end
 
-
-function trig_map(D::Dict{String, Vector{Vector{String}}}, E::Dict{String, Vector{Vector{Vector{UInt64}}}})
-    # Keep track of lengths of part of URL somehow
-    for key in keys(D)
-        for i=1:length(D[key])
-            if(isempty(D[key][i]))
-                    push!(E[key], [[0]])
-                    continue 
-            end
-            push!(E[key], transform_to_trig(D[key][i])) # transfrom to trig every single vector
-        end
-    end
-end
-#trig_map(dict, dict_hashed)
-
-# Transform to hist. vector
-function to_hist_vec(D::Dict{String,Vector{Vector{Vector{UInt64}}}})
-    for key in keys(D)
-        for i=1:length(D[key])
-            for j=1:length(D[key][i])
-                 D[key][i][j] = map_to_vec(D[key][i][j])
-            end
-        end
-    end
-end
-
-
-#to_hist_vec(dict_hashed)
 
 function count_size(key::String)
     return sum(bag_indx_parts[key])
